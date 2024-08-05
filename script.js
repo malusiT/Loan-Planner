75c279158c01d7aca1d97ce04cbf86bc338f2d79
Malusi <mhlangamalusi23@gmail.com>
2024-08-05 21:38:42 +0200

    first commit with project files

---
+++ script.js
@@ -0,0 +1,43 @@
+const outputText = document.querySelector("#output-amount")
+
+const calculateBtn = document.querySelector("#calculate-button");
+const resetButton = document.querySelector("#reset"); 
+ 
+function calculate(value){
+    
+    let interestRate = document.querySelector("#interest-rate").value
+    interestRate = parseFloat(interestRate)
+
+    let loanPeriod = document.querySelector("#months").value;
+    loanPeriod = parseFloat(loanPeriod)
+
+    let adminFee = document.querySelector("#admin-fee").value;
+    adminFee = parseFloat(adminFee)
+
+    return (value + adminFee + (value * (interestRate/100) * loanPeriod)).toFixed(2);
+}
+
+calculateBtn.addEventListener("click", () =>{
+    let loanAmount = document.querySelector("#loan-amount").value
+    loanAmount = parseFloat(loanAmount)
+    
+    let interestRate = parseFloat(document.querySelector("#interest-rate").value);
+    let adminFee = parseFloat(document.querySelector("#admin-fee").value);
+    let loanPeriod = parseFloat(document.querySelector("#months").value);
+
+    if (isNaN(loanAmount) || isNaN(interestRate) || isNaN(adminFee) || isNaN(loanPeriod)) {
+        outputText.textContent = "Please enter valid numbers in all fields.";
+    } else {
+        outputText.textContent = `R${loanAmount} -> R${calculate(loanAmount)}`;
+    }
+})
+ 
+resetButton.addEventListener("click", () =>{
+    
+    document.querySelector("#loan-amount").value = ""
+    document.querySelector("#interest-rate").value = "30";
+    document.querySelector("#admin-fee").value ="30"
+    document.querySelector("#months").value = "1"; // Reset to the first option in the select element
+    outputText.textContent = "0"
+
+})
