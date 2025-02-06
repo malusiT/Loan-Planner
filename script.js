de6c9b50e550fe0ed5c1a0e67f7dce0758fb0709
Malusi Mhlanga <mhlangamalusi23@gmail.com>
2025-02-06 13:02:22 +0200

    Fixed two month loan calulations

--- script.js
+++ script.js
@@ -3,18 +3,42 @@ const outputText = document.querySelector("#output-amount")
 const calculateBtn = document.querySelector("#calculate-button");
 const resetButton = document.querySelector("#reset"); 
  
-function calculate(value){
+function calculateForOne(value){
     
     let interestRate = document.querySelector("#interest-rate").value
-    interestRate = parseFloat(interestRate)
-
-    let loanPeriod = document.querySelector("#months").value;
-    loanPeriod = parseFloat(loanPeriod)
+    interestRate = parseFloat(interestRate);
 
     let adminFee = document.querySelector("#admin-fee").value;
-    adminFee = parseFloat(adminFee)
+    adminFee = parseFloat(adminFee);
 
-    return (value + adminFee + (value * (interestRate/100) * loanPeriod)).toFixed(2);
+    return (value + adminFee + (value * (interestRate/100))).toFixed(2);
+}
+
+function calculateForTwo(value){
+    let interestRate = document.querySelector("#interest-rate").value
+    interestRate = parseFloat(interestRate);
+
+
+    let adminFee = document.querySelector("#admin-fee").value;
+    adminFee = parseFloat(adminFee);
+    //eg value = R 2000
+
+    let initRepayment = Number((value + adminFee + (value * (interestRate/100))).toFixed(2)); // R 1330
+    console.log(initRepayment);
+
+    let divByTwo = initRepayment / 2; // R 1330
+    console.log(divByTwo);
+
+    let addedTwoMonthInterest = divByTwo * (interestRate/100); // R 394.5
+    console.log(addedTwoMonthInterest);
+
+    let totalForSecondMonth = addedTwoMonthInterest + divByTwo; // R 1709.5
+    console.log(totalForSecondMonth);
+
+    let totalLoanInterest = totalForSecondMonth + divByTwo; //R 3024.5
+    console.log( totalLoanInterest);
+
+    return totalLoanInterest.toFixed(2);
 }
 
 calculateBtn.addEventListener("click", () =>{
@@ -25,14 +49,17 @@ calculateBtn.addEventListener("click", () =>{
     let adminFee = parseFloat(document.querySelector("#admin-fee").value);
     let loanPeriod = parseFloat(document.querySelector("#months").value);
 
+    
+
     if (isNaN(loanAmount) || isNaN(interestRate) || isNaN(adminFee) || isNaN(loanPeriod)) {
         outputText.textContent = "Please enter valid numbers in all fields.";
     }else {
         if (loanPeriod === 1){
-            outputText.textContent = `Loan Repayment for R${loanAmount} = R${calculate(loanAmount)}`;
+            outputText.textContent = `Loan Repayment for R${loanAmount} = R${calculateForOne(loanAmount)}`;
         }else if(loanPeriod === 2){
-            outputText.textContent = `Loan Repayment for R${loanAmount} = R${calculate(loanAmount)}`;
-            outputText.textContent += `\n R${calculate(loanAmount)/2} per month`
+            let totalRepaymentForTwo = calculateForTwo(loanAmount);
+            outputText.textContent = `Loan Repayment for R${loanAmount} = R${totalRepaymentForTwo}`;
+            outputText.textContent += `\n R${(totalRepaymentForTwo/2).toFixed(2)} per month `
         }
         
     }
